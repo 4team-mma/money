e4f22c3ac013080284caf8af8c8c36d16b99c8b3
吳沛青 <>
2025-12-23 10:41:11 +0800

    新增v-calendar

--- src/view/Book.vue
+++ src/view/Book.vue
@@ -1,14 +1,22 @@
 <script setup>
-import Nav from '@/components/Nav.vue';
+    import Nav from '@/components/Nav.vue';
+
+    import { Calendar, DatePicker } from 'v-calendar';
+    import 'v-calendar/style.css';
+    import { ref } from 'vue';
+
+    // DatePicker 綁定的日期
+    const date = ref(new Date());
 </script>
+
 <template>
-<Nav>
-<h1>行事曆</h1>
-
-
-</Nav>
-
+    <Nav>
+        <h1>行事曆</h1>
+        <!-- 日期選擇器 --><DatePicker v-model="date" />
+        <!-- 月曆 --><Calendar />
+    </Nav>
 </template>
+
 <style scoped>
-    
+
 </style>
