20d619373b363038bf982f7815067505c9c03299
xu3ru8p <1516799593@qq.com>
2025-12-23 09:38:08 +0800

    julia_update

--- src/view/Chart.vue
+++ src/view/Chart.vue
@@ -2,13 +2,27 @@
 import Nav from '@/components/Nav.vue';
 </script>
 <template>
-<Nav>
-<h1>我是圖表分析頁面</h1>
+    <Nav>
+        <h2>圖表分析</h2>
+        <div class="out">
+            <div class="btn-group t-btn-group" role="group" aria-label="Basic outlined example">
+                <RouterLink class="btn btn-outline-primary" to="/Add">淨資產趨勢</RouterLink>
+                <RouterLink class="btn btn-outline-primary" to="/Inn">收支趨勢</RouterLink>
+                <RouterLink class="btn btn-outline-primary" to="/Trans">支出分析</RouterLink>
+                <RouterLink class="btn btn-outline-primary" to="/Trans">收入分析</RouterLink>
+            </div>
+        </div>
 
 
 
-</Nav>
+
+
+    </Nav>
 </template>
 <style scoped>
-    
+h2 {
+    text-align: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
 </style>
